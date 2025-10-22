90ae6e3a240acc45d5f8d77f6e3ba7e1dec8263a
yoonjin <yoonjin327@gmail.com>
2025-10-22 19:54:45 +0900

    2

---
+++ src/Best.java
@@ -0,0 +1,6 @@
+public class Best {
+    public void main(String [] args) {
+        System.out.println("학기 시작되었어요!");
+        System.out.println("이번 학기 자바를 열심히 공부해 최고의 Software 개발자가 될 거예요~~");
+    }
+}
